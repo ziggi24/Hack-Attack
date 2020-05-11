8eb0506caaa853ec6254f2e35be5b130aabb9cf5
Zach <ziggi24@gmail.com>
2020-05-11 12:44:13 -0700

    added button listeners to get the current selections

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,30 @@
 console.log("connected");
+const buttonOptions = [];
+const checkOptions = [];
+const attackOptions = [];
+const targetOptions = [];
+let currentSelector = "";
+let currentCheckbox = "";
+let currentButton = "";
+
 $('button.launch').click(function (event) {
-    console.log("Clicked!" + event.target);
+    console.log("Launching Attack!");
 })
 $('div.buttons').click(function (event) {
     //console.log("clicked!");
     if ($(event.target).hasClass('nes-btn')) {
-        console.log("clicked a button!");
-        console.log($(event));
+        currentButton = $(event.target)[0].innerText
+        console.log("button clicked - " + currentButton);
     }
 })
 $('div.selectors').click(function (event) {
-    console.log("clicked " + event);
+
+    console.log("clicked " + $(event.target).selectedIndex);
+
+
 })
+$('input').on('change', function (event) {
+    $('input').not(this).prop('checked', false);
+    currentCheckbox = $(event.target).next()[0].innerText;
+    console.log("checkbox - " + currentCheckbox);
+});
